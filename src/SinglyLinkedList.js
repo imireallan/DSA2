ebc0a4db9d551a2a439e1866831b91ff4235296b
Allan Imire <allanimire@gmail.com>
2024-03-18 09:21:24 +0300

    add jsdoc

--- src/SinglyLinkedList.js
+++ src/SinglyLinkedList.js
@@ -10,6 +10,12 @@ class SinglyLinkedList {
     this.head = this.tail = null;
     this.length = 0;
   }
+
+  /**
+   * Adds a new node with the given value to the end of the linked list.
+   * @param {*} val - The value to be added to the linked list.
+   * @returns {LinkedList} - The updated linked list.
+   */
   push(val) {
     const node = new Node(val);
     if (!this.head) {
@@ -21,6 +27,11 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  /**
+   * Removes the last element from the linked list.
+   * @returns {any} The removed element.
+   */
   pop() {
     let current = this.head;
     let newTail = current;
@@ -37,6 +48,7 @@ class SinglyLinkedList {
     }
     return current;
   }
+
   shift() {
     let current = this.head;
     let newHead = current.next;
@@ -47,6 +59,7 @@ class SinglyLinkedList {
     }
     return current;
   }
+
   unshift(val) {
     const node = new Node(val);
     if (!this.head) {
@@ -58,6 +71,13 @@ class SinglyLinkedList {
     }
     this.length++;
   }
+
+  /**
+   * Retrieves the element at the specified position in the linked list.
+   * @param {number} position - The position of the element to retrieve.
+   * @returns {any} The element at the specified position.
+   * If the position is invalid or the linked list is empty, returns undefined.
+   */
   get(position) {
     if (!this.head) return;
     if (position < 0 || position >= this.length) return;
@@ -72,6 +92,12 @@ class SinglyLinkedList {
     return current;
   }
 
+  /**
+   * Sets the value at the specified position in the data structure.
+   * @param {number} position - The position at which to set the value.
+   * @param {*} val - The value to set.
+   * @returns {boolean} - True if the value was successfully set, false otherwise.
+   */
   set(position, val) {
     const current = this.get(position);
     if (current) {
@@ -95,6 +121,11 @@ class SinglyLinkedList {
     this.length++;
     return false;
   }
+  /**
+   * Removes a node at the specified position in the linked list.
+   * @param {number} position - The position of the node to be removed.
+   * @returns {Node|null} - The removed node, or null if the position is invalid.
+   */
   remove(position) {
     if (position < 0 || position >= this.length) return;
     if (position === 0) return this.shift();
@@ -104,10 +135,13 @@ class SinglyLinkedList {
     const removed = prevNode.next;
 
     prevNode.next = removed.next;
-    removed.next = null
+    removed.next = null;
     this.length--;
     return removed;
   }
+  /**
+   * Reverses the linked list.
+   */
   reverse() {
     let currentNode = this.head;
     this.head = this.tail;
